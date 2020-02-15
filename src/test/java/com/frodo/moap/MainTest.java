efef935097e27a60f30eca3fcb63ae2d7a711cf4
Frodo <Frodo@Bagend.com>
2020-02-15 17:14:47 +0100

    refactored tests

--- src/test/java/com/frodo/moap/MainTest.java
+++ src/test/java/com/frodo/moap/MainTest.java
@@ -1,5 +1,6 @@
 package com.frodo.moap;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -13,12 +14,19 @@ import static org.hamcrest.core.IsEqual.equalTo;
 public class MainTest {
 
     Main main = new Main();
+    MainTestData data = new MainTestData();
+
+    @Before
+    public void setup() {
+        data = new MainTestData();
+
+    }
 
     @Test
     public void removeColumnsOk() {
 
-        List<Attribute> attributes = createAttributes();
-        List<Column> columns = createColumns();
+        List<Attribute> attributes = data.createAttributes();
+        List<Column> columns = data.createColumns();
 
         main.removeColumnsFromAttributes4(columns, attributes);
 
@@ -35,8 +43,8 @@ public class MainTest {
     @Test
     public void removeColumnsOk2() {
 
-        List<Attribute> attributes = createAttributes2();
-        List<Column> columns = createColumns2();
+        List<Attribute> attributes = data.createAttributes2();
+        List<Column> columns = data.createColumns2();
 
         main.removeColumnsFromAttributes4(columns, attributes);
 
@@ -53,8 +61,8 @@ public class MainTest {
     @Test
     public void removeColumnsOk3() {
 
-        List<Attribute> attributes = createAttributes3();
-        List<Column> columns = createColumns3();
+        List<Attribute> attributes = data.createAttributes3();
+        List<Column> columns = data.createColumns3();
 
         main.removeColumnsFromAttributes4(columns, attributes);
 
@@ -67,90 +75,5 @@ public class MainTest {
 
     }
 
-    private List<Attribute> createAttributes() {
-        List<Attribute> attributes = new ArrayList();
-        Attribute attribute1 = Attribute.builder().aName("name1").build();
-        Attribute attribute2 = Attribute.builder().aName("name2").build();
-        Attribute attribute3 = Attribute.builder().aName("name3").build();
-        attributes.add(attribute1);
-        attributes.add(attribute2);
-        attributes.add(attribute3);
 
-        return attributes;
-
-    }
-
-
-    private List<Column> createColumns() {
-        List<Column> columns = new ArrayList();
-        Column column1 = Column.builder().cName("name1").build();
-        Column column2 = Column.builder().cName("name2").build();
-        Column column3 = Column.builder().cName("name4").build();
-        columns.add(column1);
-        columns.add(column2);
-        columns.add(column3);
-
-        return columns;
-
-    }
-
-    private List<Attribute> createAttributes2() {
-        List<Attribute> attributes = new ArrayList();
-        Attribute attribute1 = Attribute.builder().aName("name1").build();
-        Attribute attribute2 = Attribute.builder().aName("name2").build();
-        Attribute attribute3 = Attribute.builder().aName("name3").build();
-        Attribute attribute4 = Attribute.builder().aName("name4").build();
-        Attribute attribute5 = Attribute.builder().aName("name5").build();
-        attributes.add(attribute1);
-        attributes.add(attribute2);
-        attributes.add(attribute3);
-        attributes.add(attribute4);
-        attributes.add(attribute5);
-
-        return attributes;
-
-    }
-
-    private List<Column> createColumns2() {
-        List<Column> columns = new ArrayList();
-        Column column99 = Column.builder().cName("name99").build();
-        Column column100 = Column.builder().cName("name100").build();
-        Column column101 = Column.builder().cName("name101").build();
-        columns.add(column99);
-        columns.add(column100);
-        columns.add(column101);
-
-        return columns;
-
-    }
-
-    private List<Attribute> createAttributes3() {
-        List<Attribute> attributes = new ArrayList();
-        Attribute attribute1 = Attribute.builder().aName("name1").build();
-        Attribute attribute2 = Attribute.builder().aName("name2").build();
-        Attribute attribute3 = Attribute.builder().aName("name3").build();
-        Attribute attribute4 = Attribute.builder().aName("name4").build();
-        Attribute attribute5 = Attribute.builder().aName("name5").build();
-        attributes.add(attribute1);
-        attributes.add(attribute2);
-        attributes.add(attribute3);
-        attributes.add(attribute4);
-        attributes.add(attribute5);
-
-        return attributes;
-
-    }
-
-    private List<Column> createColumns3() {
-        List<Column> columns = new ArrayList();
-        Column column99 = Column.builder().cName("name99").build();
-        Column column1 = Column.builder().cName("name1").build();
-        Column column101 = Column.builder().cName("name101").build();
-        columns.add(column99);
-        columns.add(column1);
-        columns.add(column101);
-
-        return columns;
-
-    }
 }
---
+++ src/test/java/com/frodo/moap/MainTestData.java
@@ -0,0 +1,94 @@
+package com.frodo.moap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainTestData {
+
+    protected List<Attribute> createAttributes() {
+        List<Attribute> attributes = new ArrayList();
+        Attribute attribute1 = Attribute.builder().aName("name1").build();
+        Attribute attribute2 = Attribute.builder().aName("name2").build();
+        Attribute attribute3 = Attribute.builder().aName("name3").build();
+        attributes.add(attribute1);
+        attributes.add(attribute2);
+        attributes.add(attribute3);
+
+        return attributes;
+
+    }
+
+
+    protected List<Column> createColumns() {
+        List<Column> columns = new ArrayList();
+        Column column1 = Column.builder().cName("name1").build();
+        Column column2 = Column.builder().cName("name2").build();
+        Column column3 = Column.builder().cName("name4").build();
+        columns.add(column1);
+        columns.add(column2);
+        columns.add(column3);
+
+        return columns;
+
+    }
+
+    protected List<Attribute> createAttributes2() {
+        List<Attribute> attributes = new ArrayList();
+        Attribute attribute1 = Attribute.builder().aName("name1").build();
+        Attribute attribute2 = Attribute.builder().aName("name2").build();
+        Attribute attribute3 = Attribute.builder().aName("name3").build();
+        Attribute attribute4 = Attribute.builder().aName("name4").build();
+        Attribute attribute5 = Attribute.builder().aName("name5").build();
+        attributes.add(attribute1);
+        attributes.add(attribute2);
+        attributes.add(attribute3);
+        attributes.add(attribute4);
+        attributes.add(attribute5);
+
+        return attributes;
+
+    }
+
+    protected List<Column> createColumns2() {
+        List<Column> columns = new ArrayList();
+        Column column99 = Column.builder().cName("name99").build();
+        Column column100 = Column.builder().cName("name100").build();
+        Column column101 = Column.builder().cName("name101").build();
+        columns.add(column99);
+        columns.add(column100);
+        columns.add(column101);
+
+        return columns;
+
+    }
+
+    protected List<Attribute> createAttributes3() {
+        List<Attribute> attributes = new ArrayList();
+        Attribute attribute1 = Attribute.builder().aName("name1").build();
+        Attribute attribute2 = Attribute.builder().aName("name2").build();
+        Attribute attribute3 = Attribute.builder().aName("name3").build();
+        Attribute attribute4 = Attribute.builder().aName("name4").build();
+        Attribute attribute5 = Attribute.builder().aName("name5").build();
+        attributes.add(attribute1);
+        attributes.add(attribute2);
+        attributes.add(attribute3);
+        attributes.add(attribute4);
+        attributes.add(attribute5);
+
+        return attributes;
+
+    }
+
+    protected List<Column> createColumns3() {
+        List<Column> columns = new ArrayList();
+        Column column99 = Column.builder().cName("name99").build();
+        Column column1 = Column.builder().cName("name1").build();
+        Column column101 = Column.builder().cName("name101").build();
+        columns.add(column99);
+        columns.add(column1);
+        columns.add(column101);
+
+        return columns;
+
+    }
+}
